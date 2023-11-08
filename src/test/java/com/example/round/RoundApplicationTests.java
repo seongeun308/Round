4e430df008299d5774cf88d831b444b9f678a72d
seongeun <kse011010@gmail.com>
2023-11-08 15:08:16 +0900

    first commit

---
+++ src/test/java/com/example/round/RoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.round;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
